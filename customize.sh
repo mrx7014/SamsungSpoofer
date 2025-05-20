a516c6d86b666117008ddd1cf13bf898f49f7edd
MRX7014 | Octopus <safen894@gmail.com>
2025-05-21 01:09:58 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,17 +1,7 @@
-#!/system/bin/sh
-
-# Module variables
-MODPATH_SYSTEM_PROP="$MODPATH"/system.prop
-MOD_PROP_MODEL=$(grep_prop ro.product.model "$MODPATH_SYSTEM_PROP")
-MOD_PROP_PRODUCT=$(grep_prop ro.build.product "$MODPATH_SYSTEM_PROP" | tr '[:lower:]' '[:upper:]')
-MOD_PROP_VERSION=$(grep_prop ro.build.version.release "$MODPATH_SYSTEM_PROP")
-MOD_PROP_SECURITYPATCH=$(grep_prop ro.build.version.security_patch "$MODPATH_SYSTEM_PROP")
-MOD_PROP_VERSIONCODE=$(date -d "$MOD_PROP_SECURITYPATCH" '+%y%m%d')
-MOD_PROP_MONTH=$(date -d "$MOD_PROP_SECURITYPATCH" '+%B')
-MOD_PROP_YEAR=$(date -d "$MOD_PROP_SECURITYPATCH" '+%Y')
-
-# Print head message
-ui_print "- Installing, $MOD_PROP_MODEL ($MOD_PROP_PRODUCT) Prop - $MOD_PROP_MONTH $MOD_PROP_YEAR"
-
-# Print footer message
-ui_print "- Script by MRX7014, Telegram: @MRX7014"
+ui_print "┏━━━[ S24 Ultra Spoofer ]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+ui_print "┃ Spoof Your Device To S24 Ultra Spoofer [S928BXXU4BYD9] (OneUI 7.0) ┃"
+ui_print "┃ v6.0.0 - AllDevices                                                ┃"
+ui_print "┃ By: MRX7014                                                        ┃"
+ui_print "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+sleep 3
+ui_print "✨ Done, Enjoy, Don't forget to join MRX7014 Cloud"
